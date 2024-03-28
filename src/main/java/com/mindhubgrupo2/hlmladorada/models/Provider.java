0d4d46186629f9e62325c01b135ac04dd2be9d5d
OrlandoConci <orlandomarceloconci@gmail.com>
2024-03-28 10:49:34 -0300

    RegisterDTO y purchase

--- src/main/java/com/mindhubgrupo2/hlmladorada/models/Provider.java
+++ src/main/java/com/mindhubgrupo2/hlmladorada/models/Provider.java
@@ -1,4 +1,98 @@
 package com.mindhubgrupo2.hlmladorada.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Provider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long providerID;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String ruc;
+
+    private String adress;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "PurchaseProvider")
+    private Purchase purchaseHolder;
+
+    public Provider() {
+    }
+
+    public Long getProviderID() {
+        return providerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public Purchase getPurchaseHolder() {
+        return purchaseHolder;
+    }
+
+    public void setPurchaseHolder(Purchase purchaseHolder) {
+        this.purchaseHolder = purchaseHolder;
+    }
+
+    public void addPurchase(Purchase purchase) {
+        purchase.setProviderHolder(this);
+        this.setPurchaseHolder(purchase);
+    }
+
+    @Override
+    public String toString() {
+        return "Provider{" +
+                "providerID=" + providerID +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", ruc='" + ruc + '\'' +
+                ", adress='" + adress + '\'' +
+                ", purchaseHolder=" + purchaseHolder +
+                '}';
+    }
 }
--- src/main/java/com/mindhubgrupo2/hlmladorada/models/Purchase.java
+++ src/main/java/com/mindhubgrupo2/hlmladorada/models/Purchase.java
@@ -1,4 +1,112 @@
 package com.mindhubgrupo2.hlmladorada.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long purchaseID;
+
+    private int quantity;
+
+    private String details;
+
+    private LocalDateTime date;
+
+    private Double unitCost;
+
+    private Double totalCost;
+
+    @OneToOne(mappedBy = "purchaseHolder")
+    private Product productHolder;
+
+    @OneToOne(mappedBy = "purchaseHolder")
+    private Provider providerHolder;
+
+    public Purchase() {
+    }
+
+    public Purchase(int quantity, String details, LocalDateTime date, Double unitCost, Double totalCost) {
+        this.quantity = quantity;
+        this.details = details;
+        this.date = date;
+        this.unitCost = unitCost;
+        this.totalCost = totalCost;
+    }
+
+    public Long getPurchaseID() {
+        return purchaseID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Double getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Double unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public Double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(Double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public Product getProductHolder() {
+        return productHolder;
+    }
+
+    public void setProductHolder(Product productHolder) {
+        this.productHolder = productHolder;
+    }
+
+    public Provider getProviderHolder() {
+        return providerHolder;
+    }
+
+    public void setProviderHolder(Provider providerHolder) {
+        this.providerHolder = providerHolder;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "purchaseID=" + purchaseID +
+                ", quantity=" + quantity +
+                ", details='" + details + '\'' +
+                ", date=" + date +
+                ", unitCost=" + unitCost +
+                ", totalCost=" + totalCost +
+                '}';
+    }
 }
